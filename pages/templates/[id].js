b47a28a35e44626f2c3356c54064e8dfbc87f262
keiraeleison <keiraeleison@gmail.com>
2024-05-29 01:04:54 +0200

    Adding dynamic routing - getStaticPaths

--- pages/templates/[id].js
+++ pages/templates/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+
+    const paths = data.map(template => {
+        return {
+            params: { id: template.id.toString() }
+        }
+    })
+
+    return {
+        paths: paths
+    }
+}
+
 const Details = () => {
     return ( 
         <div>
